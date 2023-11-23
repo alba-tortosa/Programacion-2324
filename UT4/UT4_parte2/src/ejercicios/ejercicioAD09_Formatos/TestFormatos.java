589f2fb7ab64c8d04f6b867096e46dd5f5addc02
alba-tortosa <albatortosalopez@educacion.navarra.es>
2023-11-23 13:43:06 +0100

    ut4 - ejercicio AD09

--- UT4/UT4_parte2/src/ejercicios/ejercicioAD09_Formatos/TestFormatos.java
+++ UT4/UT4_parte2/src/ejercicios/ejercicioAD09_Formatos/TestFormatos.java
@@ -1,20 +1,56 @@
 package ejercicios.ejercicioAD09_Formatos;
 
+import java.util.Scanner;
+
 public class TestFormatos {
 
     public static void main(String[] args) {
 
-        formato1();
+        //formato1();
+        formato2();
 
 
     }
-    
+
     private static void formato1() {
         // | CATEGORIA | NOMBRE | BITS |
-        System.out.printf("| %10s | %-8s | %4s |", "CATEGORIA", "NOMBRE", "BITS");
+        //--------------------------------
+        System.out.printf("%s\n", "-".repeat(32));
+        System.out.printf("   %s\n", "Tipos Primitivos en Java");
+        System.out.printf(" %s\n", "(ejemplo de tabla con printf)");
+        System.out.printf("%s\n", "-".repeat(32));
+        System.out.printf("| %10s | %-8s | %4s |\n", "CATEGORIA", "NOMBRE", "BITS");
+        System.out.printf("%s\n", "-".repeat(32));
+        System.out.printf("| %-10s | %-8s | %04d |\n", "Decimal", "double", 64);
     }
 
+    private static void formato2() {
 
+        Scanner scanner = new Scanner(System.in);
+
+        String linea = "+----+------------+-----------+------+--------+\n";
+        String tabla = linea;
+        tabla += String.format("| %2s | %-10S | %9S | %-4s | %6s |\n", "ID", "NOMBRE", "APELLIDO", "EDAD", "ALTURA");
+        tabla += linea;
+        for (int i = 0; i < 5; i++) {
+            //Pide al usuario un nombre, un apellido, una edad y una altura
+            System.out.printf("%10s","Nombre:");
+            String nombre = scanner.nextLine();
+            System.out.printf("%10s","Apellidos:");
+            String apellido = scanner.nextLine();
+            System.out.printf("%10s","Edad:");
+            int edad = scanner.nextInt();
+            System.out.printf("%10s","Altura:");
+            double altura = scanner.nextDouble();
+            scanner.nextLine();
+            //| 1 | MARIA | MARTÍNEZ | 34 | 1,67 |
+            tabla +=String.format("| %2d | %-10S | %9S | %-4d | %6.2f |\n", i+1, nombre, apellido, edad, altura);
+
+        }
+        tabla += linea;
+        System.out.println(tabla);
+
+    }
 
     private static void ejemplos() {
         int contador = 2;
