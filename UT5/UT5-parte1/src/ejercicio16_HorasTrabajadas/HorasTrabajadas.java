ea71bdb2eef764984235189513f9ab8e9ac170fa
alba-tortosa <albatortosalopez@educacion.navarra.es>
2024-01-24 09:59:06 +0100

    UT5-parte1 HorasTrabajadas y MatrizIdentidad

--- UT5/UT5-parte1/src/ejercicio16_HorasTrabajadas/HorasTrabajadas.java
+++ UT5/UT5-parte1/src/ejercicio16_HorasTrabajadas/HorasTrabajadas.java
@@ -1,22 +1,53 @@
 package ejercicio16_HorasTrabajadas;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class HorasTrabajadas {
 
     public static void main(String[] args) {
 
-        // a) Define y crea (en un solo paso) un array bidimensional, horasTrabajadas, que permita almacenar las horas que han trabajado en cada uno de los 7 días de la semana los 15 trabajadores de una empresa (las filas son los días y las columnas los trabajadores)
+        // a) Define y crea (en un solo paso) un array bidimensional, horasTrabajadas, que permita almacenar las horas
+        // que han trabajado en cada uno de los 7 días de la semana los 15 trabajadores de una empresa
+        // (las filas son los días y las columnas los trabajadores)
 
+        int[][] horasTrabajadas = new int[7][15];
 
         // b) Define un array de valores constantes con los nombres de los 7 días de la semana
-
+        String diasSemana[] = {"LUNES","MARTES","MIÉRCOLES","JUEVES","VIERNES","SÁBADO","DOMINGO"};
 
         // c) Inicializa el array horasTrabajadas con valores aleatorios entre 8 y 12
+        Random random = new Random();
+        for (int i = 0; i < horasTrabajadas.length; i++) {
+            for (int j = 0; j < horasTrabajadas[i].length; j++) {
+                horasTrabajadas[i][j]=random.nextInt(8,13);
+            }
+        }
 
+        // d) Calcula las horas trabajadas en cada día de la semana por todos los trabajadores
+        // (necesitarás un array adicional)
+        int cantidad = 0;
+        int[] horasPorDia=new int[horasTrabajadas.length];
+//        for (int i = 0; i < horasTrabajadas.length; i++) {//miramos dia
+//            for (int j = 0; j < horasTrabajadas[i].length; j++) {//miramos cada celda de cada dia(fila)
+//                cantidad+= horasTrabajadas[i][j];
+//            }
+//            horasPorDia[i]= cantidad;
+//            cantidad = 0;
+//        }
 
-        // d) Calcula las horas trabajadas en cada día de la semana por todos los trabajadores (necesitarás un array adicional)
+        for (int i = 0; i < horasTrabajadas.length; i++) {//miramos dia
+            for (int j = 0; j < horasTrabajadas[i].length; j++) {//miramos cada celda de cada dia(fila)
+                horasPorDia[i]+= horasTrabajadas[i][j];
+            }
+        }
 
 
         // e) Escribe el nombre del día de la semana y el total de horas trabajadas calculadas en el apartado anterior
+        for (int i = 0; i < diasSemana.length; i++) {
+            System.out.println(Arrays.toString(horasTrabajadas[i]));
+            System.out.println(diasSemana[i] + "  " +horasPorDia[i]);
+        }
 
 
 
--- UT5/UT5-parte1/src/ejercicio17_MatrizIdentidad/MatrizIdentidad.java
+++ UT5/UT5-parte1/src/ejercicio17_MatrizIdentidad/MatrizIdentidad.java
@@ -2,12 +2,23 @@ package ejercicio17_MatrizIdentidad;
 
 public class MatrizIdentidad {
 
+    public static final int NUMERO = 1;
+
     public static void main(String[] args) {
-        int[][] matriz = generarMatrizIdentidad(6);
+        int[][] matriz = generarMatrizIdentidad(8);
 
         for (int[] fila : matriz) {
+            // fila = matriz[i]
             for (int celda : fila) {
+                // celda = fila[j] = matriz[i][j]
                 System.out.print(celda + "  ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "  ");
             }
             System.out.println();
         }
@@ -16,7 +27,13 @@ public class MatrizIdentidad {
 
     // Utiliza un bucle para rellenar los valores
     public static int[][] generarMatrizIdentidad(int dimension) {
+        int[][] longitudMatrix = new int[dimension][dimension];
 
-        return null;
+        for (int i = 0; i < longitudMatrix.length ; i++) {
+            for (int j = 0; j < longitudMatrix[0].length ; j++) {
+                if(i == j) longitudMatrix[i][j] = NUMERO;
+            }
+        }
+        return longitudMatrix;
     }
 }
