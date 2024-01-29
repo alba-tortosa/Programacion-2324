dc1895acc4e3e66499a9ae87db7b7e8aed5f6582
alba-tortosa <albatortosalopez@educacion.navarra.es>
2024-01-29 12:31:54 +0100

    UT5-parte2-ejercicio24_Palabra

--- UT5/UT5-parte2/src/ejercicio23_String/Main.java
+++ UT5/UT5-parte2/src/ejercicio23_String/Main.java
@@ -14,25 +14,32 @@ public class Main {
 
         //Asigna a resul y/o a carácter y/o a pos:
         // 1. la cadena str convertida a mayúsculas
-
+        resul = str.toUpperCase();
+        System.out.println("str="+ str + " resul="+resul);
         // 2. el carácter de la posición 6 de str
-
+        caracter = str.charAt(6);
+        System.out.println("el carácter de la posición 6 de str: " + caracter);
         // 3. el último carácter de la cadena str
-
+        caracter = str.charAt(str.length()-1);
+        System.out.println("el último carácter de la cadena str:" + caracter);
         // 4. compara str con la cadena resul sin tener en cuenta mayúsculas ni minúsculas
-
+        System.out.println("son iguales sin tener en cuenta mayus/minus? " + str.equalsIgnoreCase(resul));
+        System.out.println("son iguales teniendo en cuenta mayus/minus? " + str.equals(resul));
         // 5. pregunta si str empieza por “Ba”
-
+        System.out.println("pregunta si str empieza por “Ba”? " + str.startsWith("Ba"));
         // 6. sustituye en str todas las 'e' por '*'
-
+        resul = str.replace('e', '*');
         // 7. devuelve la primera aparición de la 'd' en str
-
+        pos = str.indexOf("d");
         // 8. localiza la última aparición de la 'c' en str. Extrae , a partir de ahí, la subcadena existente hasta
         // el final.
-
+        pos = str.lastIndexOf("c");
+        resul = str.substring(pos);
         // 9. convierte a String el valor 66
-
+        resul = String.valueOf(66);
         //10. pregunta si la cadena str es vacía
-
+        System.out.println(resul.isEmpty());
+        System.out.println(resul.equals(""));
+        System.out.println(resul == null);
     }
 }
--- UT5/UT5-parte2/src/ejercicio24_Palabra/Palabra.java
+++ UT5/UT5-parte2/src/ejercicio24_Palabra/Palabra.java
@@ -22,7 +22,15 @@ public class Palabra {
      * @return la palabra con guiones
      */
     public String guionizar() {
-        return null;
+        String palabraGuiones = "";
+        for (int i = 0; i < palabra.length() ; i++) {
+            if(i == palabra.length()-1){
+                palabraGuiones += this.palabra.charAt(i);
+            }else{
+                palabraGuiones += this.palabra.charAt(i) + "-";
+            }
+        }
+        return palabraGuiones;
     }
 
 
@@ -30,11 +38,21 @@ public class Palabra {
      * @return el numero de vocales de la palabra
      */
     public int contarVocales() {
-        return 0;
+        int contador = 0;
+        for (int i = 0; i < palabra.length(); i++) {
+            if (esVocal(palabra.charAt(i))) {
+                contador++;
+            }
+        }
+        return contador;
     }
 
     private boolean esVocal(char car) {
-        return false;
+        return "aeiouAEIOU".contains(String.valueOf(car));
+//        return "aeiouAEIOU".indexOf(car) != -1;
+//        return car == 'a' || car == 'e' || car == 'i' || car == 'o' || car == 'u'
+//                || car == 'A' || car == 'E' || car == 'I' || car == 'O' || car == 'U';
+
     }
 
     /**
@@ -44,7 +62,9 @@ public class Palabra {
      * @return true si son iguales, false en otro caso
      */
     public boolean mayorQue(String palabra) {
-
+        if ( this.palabra.compareTo(palabra)>0){
+            return true;
+        }
         return false;
     }
 
@@ -52,16 +72,58 @@ public class Palabra {
      * Borrar las apariciones de str en palabra
      */
     public String borrarOcurrenciasDe(String str) {
-
-        return null;
+        return this.palabra.replace(str, "");
     }
 
     public char[][] toArray2D() {
-        return null;
+        char[][] output = new char[this.palabra.length()][this.palabra.length()];
+        for (int i = 0; i < output.length; i++) {
+            for (int j = 0; j < output.length; j++) {
+                output[i][j] = (j < i) ? '*' : this.palabra.charAt(j);
+//                if (j < i) output[i][j] = '*';
+//                else output[i][j] = this.palabra.charAt(j);
+            }
+        }
+        return output;
+    }
+
+    public char[][] toArray2D_3() {
+        String p = palabra;
+        char[][] bidimensional = new char[palabra.length()][palabra.length()];
+        for (int i = 0; i < p.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                bidimensional[i][j] = p.charAt(j);
+            }
+            p = p.replaceFirst(String.valueOf(p.charAt(i)), String.valueOf(ASTERISCO));
+        }
+        return bidimensional;
+    }
+
+    public char[][] toArray2D_v2() {
+        char[][] array = new char[palabra.length()][palabra.length()];
+        int aux=0;
+        for (int i = 0; i < palabra.length(); i++) {
+            array[i][aux] ='*';
+            for (int j = 0; j < palabra.length(); j++) {
+                array[i][j] = '*';
+                for (int k = 0; k <palabra.length() ; k++) {
+                    if (j==aux) {
+                        array[k][j] = palabra.charAt(j);
+                    }
+                }
+            }
+            aux++;
+        }
+        return array;
     }
 
     public char[] toArray() {
-        return null;
+        return palabra.toCharArray();
+//        char[] array = new char[palabra.length()];
+//        for (int i = 0; i < palabra.length(); i++) {
+//            array[i] = palabra.charAt(i);
+//        }
+//        return array;
     }
 
     public void print() {
